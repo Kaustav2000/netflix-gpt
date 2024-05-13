946be523136391d00d3559ad006c27dd758552a2
Kaustav Bhattacharya <kbhattacharya75@gmail.com>
2024-05-13 05:52:11 +0530

    Build secondary container

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,3 +15,5 @@ export const API_OPTIONS = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOWRiYTliY2FmNDI1OTI1ZDFiZjJmNzIyYjhlZjhhMiIsInN1YiI6IjYxMjdkMTNhMWJmODc2MDA2MjI0YTc2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.74I_cxDoMtnoueZuviGK8wBpho0i083mOLbMHONpFzM",
   },
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
